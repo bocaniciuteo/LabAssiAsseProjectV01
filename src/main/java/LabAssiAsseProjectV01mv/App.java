d6991f77561b3c8b452e88774a8504fa7ff1e8c8
Bocaniciu Teo <bteo10@yahoo.com>
2019-03-25 16:39:01 +0200

    Initial commit

---
+++ src/main/java/LabAssiAsseProjectV01mv/App.java
@@ -0,0 +1,13 @@
+package LabAssiAsseProjectV01mv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
